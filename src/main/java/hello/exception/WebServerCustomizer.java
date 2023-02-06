61a4f1cd2e4560b06ac73fb771452acc374b6fd1
SanghwaJ <jungsh930210@gmail.com>
2023-02-06 18:01:29 +0900

    edit

--- src/main/java/hello/exception/WebServerCustomizer.java
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -6,7 +6,7 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-@Component // 스프링 빈 등록
+// @Component // 스프링 빈 등록
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
